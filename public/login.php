c07493ac590096a60e7904c97cd9b1b5a4f2a410
Alexander K <aleksandr.kuznechenkov@aio.tech>
2025-03-30 22:39:55 +0300

    feat: new stuff

--- public/login.php
+++ public/login.php
@@ -4,11 +4,6 @@ session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../bootstrap.php';
 
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->safeLoad();
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
